cd19bc4b3e3d91ed7f710c914ece3fec5d56d97b
wpineda21 <00225919@uca.edu.sv>
2020-01-13 23:52:54 -0600

    laboextra

---
+++ Laboextra/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char letter('c','o','d','i','f','i','c','a','r');
+
+    cout<<"por favor ingrese una letra"<<endl;
+    cin>>endl;
+
+    return 0;
+}
