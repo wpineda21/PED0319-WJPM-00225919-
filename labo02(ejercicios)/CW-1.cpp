bb591eb27893c35b537042db0bc2eae343ae8d1e
wpineda21 <00225919@uca.edu.sv>
2020-01-16 01:34:14 -0600

    Add excersice: actualizando ejercicio 1,2,3 con comentarios del programador

--- labo02(ejercicios)/CW-1.cpp
+++ labo02(ejercicios)/CW-1.cpp
@@ -1,83 +1,107 @@
-#include <iostream>
-#include <string>
+#include <iostream>    //libreria para entrada y salida de datos     
+#include <string>     //libreria para uso de palabras largas
 using namespace std;
 
-struct empleado{
-    int edad,salario,anoslaborales;
-    string nombre;
+//declaracion de  registro de variables
+struct employees{
+    int Age,Salary,WorkingYears;
+    string employeesName;
 };
-typedef struct empleado Empleado;
 
-void Hombre();
-void Mujer();
+//usando typedef
+typedef struct employees Staff;
 
+//prototipos de variables
+void Man();
+void Woman();
+
+
+//funcion principal
 int main(){
- int opc;
 
-cout<<"bienvenido al calculo de retiro de edad"<<endl;
-cout<<"seleccione un genero"<<endl;
-cout<<"1)Hombre..........................."<<endl;
-cout<<"2)Mujer............................"<<endl;
-cin>>opc;
+    int option;
 
-switch (opc){
-case 1: cout<<"A continuacion rellene el siguiente formulario"<<endl;
-        Hombre();
-    break;
-case 2: cout<<"A continuacion rellene el siguiente formulario"<<endl;
-        Mujer();
-    break;
+    cout<<"bienvenido al calculo de retiro de edad"<<endl;
+    cout<<"seleccione un genero"                   <<endl;
+    cout<<"1)Hombre..............................."<<endl;
+    cout<<"2)Mujer................................"<<endl;
+    cin>>option;
 
-default:cout<<"lo siento esta opcion no existe"<<endl;
-    break;
+    //menu para escoger el genero de la persona a evaluar
+    switch (option){
+        case 1: cout<<"A continuacion rellene el siguiente formulario"<<endl;
+        //llamando a la funcion man
+        
+        Man();break;   
+
+        case 2: cout<<"A continuacion rellene el siguiente formulario"<<endl;
+        
+        //llamando ala funcion woman
+        Woman();break;
+
+        //default por si algo falla o la opcion ingresada no existe    
+        default:cout<<"lo siento esta opcion no existe"<<endl;break;
+        
+    }
+    return 0; 
+    
 }
-return 0; 
-}
+//Fucion man que nos permite guardar los registros de las personas
+void Man(){
 
-void Hombre(){
-int cantidadEmpleados = 1;
-    Empleado cartera[cantidadEmpleados];
-    for (int i = 0; i < cantidadEmpleados; i++) {
+        //declarando el tamano del arreglo
+        int Quantityofemployees = 1;
+        Staff Data[Quantityofemployees];
+        
+        //entrando al ciclo for del arreglo
+        for (int i = 0; i < Quantityofemployees; i++) {
+
+        //funcion que me permite ignorar los "Enter" digitados antes de llegar aqui
         cin.ignore();
-        cout << "Nombre de empleado #" << (i+1) << ": ";
-        getline(cin, cartera[i].nombre);
-        cout << "Salario de empleado:"  << (i+1) << ": ";
-        cin >> cartera[i].salario;
-        cout << "anos laborados:" << (i+1) << ": ";
-        cin >> cartera[i].anoslaborales;
-        cout << "Edad de empleado: #" << (i+1) << ": ";
-        cin >> cartera[i].edad;
+            
+        //preguntando al usuario los datos necesarios
+        cout << "Nombre de empleado #"  << (i+1) << ": ";   getline(cin, Data[i].employeesName);
+        cout << "Salario de empleado:"  << (i+1) << ": ";   cin >> Data[i].Salary;
+        cout << "anos laborados:"       << (i+1) << ": ";   cin >> Data[i].WorkingYears;
+        cout << "Edad de empleado: #"   << (i+1) << ": ";   cin >> Data[i].Age;
         cin.ignore();
     }   
-    for (int i = 0; i < cantidadEmpleados; i++){
-        if(cartera[i].edad > 60 && cartera[i].anoslaborales > 25){
-        cout << cartera[i].nombre << " esta listo para jubilar" << endl;
+    
+        //entrando a un bucle que imprime si la persona esta lista para jubilarse o no
+        for (int i = 0; i < Quantityofemployees; i++){
+
+        //imponiendo la condicion (en este caso de edad) dentro del bucle
+        if (Data[i].Age > 60 && Data[i].WorkingYears > 25){
+        cout << Data[i].employeesName<< " esta listo para jubilar" << endl;
          }else{
         cout<<"el empleado no puede jubilarse todavia"<<endl;
+         }
+        }
     }
-    }
-}
-void Mujer(){
-int cantidadEmpleados = 1;
-    Empleado cartera[cantidadEmpleados];
-    for (int i = 0; i < cantidadEmpleados; i++) {
-         cin.ignore();
-        cout << "Nombre de empleada #" << (i+1) << ": ";
-        getline(cin, cartera[i].nombre);
-        cout << "Salario de empleada:"  << (i+1) << ": ";
-        cin >> cartera[i].salario;
-        cout << "anos laborados:" << (i+1) << ": ";
-        cin >> cartera[i].anoslaborales;
-        cout << "Edad de empleado: #" << (i+1) << ": ";
-        cin >> cartera[i].edad;
+
+//Declaracion de la funcion Mujer que me permite guardar los datos de las personas  mujeres   
+void Woman(){
+    
+        int Quantityofemployees = 1;
+        Staff Data[Quantityofemployees];
+
+        //entrando al bucle de obtencion de datos del usuario
+         for (int i = 0; i < Quantityofemployees; i++) {
+        cin.ignore();
+        cout << "Nombre de empleada #"  << (i+1) << ": ";       getline(cin, Data[i].employeesName);
+        cout << "Salario de empleada:"  << (i+1) << ": ";       cin >> Data[i].Salary;
+        cout << "anos laborados:"       << (i+1) << ": ";       cin >> Data[i].WorkingYears;
+        cout << "Edad de empleado: #"   << (i+1) << ": ";       cin >> Data[i].Age;
         cin.ignore();
     }   
-    for (int i = 0; i < cantidadEmpleados; i++){
-        if(cartera[i].edad > 55 && cartera[i].anoslaborales > 25){
-        cout << cartera[i].nombre << " esta listo para jubilar" << endl;
-    }else{
 
+        //bucle que imprime si la persona esta lista para jubilarse
+        for (int i = 0; i < Quantityofemployees; i++){
+      
+        if(Data[i].Age > 55 && Data[i].WorkingYears > 25){
+        cout << Data[i].employeesName << " esta listo para jubilar" << endl;
+        }else{
         cout<<"la empleada no puede jubilarse todavia"<<endl;
-    }
+        }
     }
 }
